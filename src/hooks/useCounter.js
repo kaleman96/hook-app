5ea4863105bd6b295e42809569ec0f76cb17869e
Kevin Aleman <kevinaleman2013@gmail.com>
2022-12-03 23:35:30 -0600

    agregando counter hook

---
+++ src/hooks/useCounter.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export const useCounter = (initialValue = 10) => {
+  const [counter, setcounter] = useState(initialValue);
+
+  const increment = () => {
+    setcounter(counter + 1);
+  };
+
+  const decrement = () => {
+    if (counter === 0) return;
+    setcounter(counter - 1);
+  };
+
+  const reset = () => {
+    setcounter(initialValue);
+  };
+
+  return {
+    counter,
+    increment,
+    decrement,
+    reset,
+  };
+};
